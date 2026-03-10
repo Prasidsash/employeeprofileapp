d0ed9876dbbe1e6f01a85d72015b167810fe87a3
Rajaprabu <aix.rajaprabu@gmail.com>
2026-03-10 18:18:59 +0530

    Versioning

--- src/Employeeprofileapp/Program.cs
+++ src/Employeeprofileapp/Program.cs
@@ -98,7 +98,7 @@ string Page(string title, string emoji, string message)
 
 app.MapGet("/", async context =>
 {
-    await context.Response.WriteAsync(Page("Home", "🏠", "Welcome to Israel-Iran War 2026"));
+    await context.Response.WriteAsync(Page("Home", "🏠", "Welcome to Indian Festival"));
 });
 
 app.MapGet("/employee", async context =>
